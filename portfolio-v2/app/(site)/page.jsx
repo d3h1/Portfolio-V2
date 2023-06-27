4ec8d5f1d898268cc3ab10ae5c82a1c188c372f6
d3h1 <denicabaravdic@gmail.com>
2023-06-27 16:17:57 -0700

    making the about page

--- portfolio-v2/app/(site)/page.jsx
+++ portfolio-v2/app/(site)/page.jsx
@@ -1,3 +1,4 @@
+import About from "../components/About";
 import Main from "../components/Main";
 import Navbar from "../components/Navbar";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <div>
       <Navbar/>
       <Main />
+      <About />
     </div>
   )
 }
